6deb8ff690ad7b7977c6462bccd12bfa89e998ba
Bob Roudebush <bob@roudybob.net>
2021-04-28 07:38:11 -0400

    sidebar and calendar integrated

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -1,27 +1,72 @@
 import * as React from 'react';
-
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
 import Sidebar from './Sidebar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 export interface MainProps {
     token: string
 }
  
 export interface MainState {
-
+    events: [
+        {
+            start: Date,
+            end: Date,
+            title: string
+        }
+    ]
 }
  
 class Main extends React.Component<MainProps, MainState> {
-    // constructor(props: MainProps) {
-    //     // super(props);
-    //     // this.state = { token: props.token  };
-    // }
+    constructor(props: MainProps) {
+        super(props);
+        this.state = { 
+            events: [
+                {
+                    start: moment().toDate(),
+                    end: moment().add(1, "days").toDate(),
+                    title: "Some title"
+                }
+            ]
+        };
+    }
+
+    
+
+    handleSelect = ({ start, end } : { start: any, end: any }) => {
+        const title = window.prompt('New Event Name');
+        console.log(title);
+        if (title) {
+            var tmpArray = this.state.events;
+            tmpArray.push({ start, end, title });
+            this.setState({ events: tmpArray });
+            console.log(this.state.events);
+        }
+    }
+
 
     render() { 
+        const localizer = momentLocalizer(moment);
+
         return (
             <div className="main-wrapper">
                 <div className="main-inner">
                     <h1>Hello from Main!</h1>
                     <p>{this.props.token}</p>
+                    <div className="calendar-wrapper">
+                        <Calendar
+                            selectable
+                            localizer={localizer}
+                            defaultView={'month'}
+                            events={this.state.events}
+                            startAccessor="start"
+                            endAccessor="end"
+                            onSelectEvent={event => alert(event.title)}
+                            onSelectSlot={this.handleSelect}
+                            style={{ height: 500 }}
+                        />
+                    </div>
                 </div>
                 <Sidebar />
             </div>
--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -1,4 +1,8 @@
 import * as React from 'react';
+import { ProSidebar, SidebarHeader, Menu, MenuItem, SidebarContent } from 'react-pro-sidebar';
+import { FaRunning, FaCalendarAlt, FaCloudDownloadAlt } from "react-icons/fa";
+import PlanModal from './PlanModal';
+import './Sidebar.scss';
 
 export interface SidebarProps {
     
@@ -14,11 +18,47 @@ class Sidebar extends React.Component<SidebarProps, SidebarState> {
     //     // this.state = { :  };
     // }
 
+    handleMenuClick = () => {
+        console.log('clicked');
+        <PlanModal className={"planentry"} buttonLabel={"Click Me"} />
+    }
+
     render() { 
         return (
-            <div className="sidebar-wrapper">
-                <h2>Hello from Sidebar</h2>
+            <div id="sidebar">
+                <ProSidebar>
+                    <Menu iconShape="square">
+                        <MenuItem icon={<FaCalendarAlt />} onClick={this.handleMenuClick}>Create Plan Entry</MenuItem>
+                        <MenuItem icon={<FaRunning />}>Record Workout</MenuItem>
+                        <MenuItem icon={<FaCloudDownloadAlt />}>Import Workout</MenuItem>
+                    </Menu>
+                    <SidebarHeader>
+                        <div className="monthlystatsheader">
+                            <h5>Monthly Stats</h5>
+                        </div>
+                    </SidebarHeader>
+                    <SidebarContent>
+                        <div className="monthlystatscontent">
+                            <p>Miles: 134</p>
+                            <p>Time: 6h4m</p>
+                            <p>Elevation: 2600ft</p>
+                        </div>
+                    </SidebarContent>
+                    <SidebarHeader>
+                        <div className="goalheader">
+                            <h5>Goal Countdown</h5>
+                        </div>
+                    </SidebarHeader>
+                    <SidebarContent>
+                        <div className="goalcontent">
+                            <p>Months: 3</p>
+                            <p>Weeks: 2</p>
+                            <p>Days: 5</p>
+                        </div>
+                    </SidebarContent>
+                </ProSidebar>
             </div>
+            
         );
     }
 }

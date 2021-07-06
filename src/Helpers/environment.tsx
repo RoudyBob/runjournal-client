f4c6422d71d129bfcbc5febe67f23287a266289f
Bob Roudebush <bob@roudybob.net>
2021-07-06 14:51:23 -0400

    fixed environment file

--- src/Helpers/environment.tsx
+++ src/Helpers/environment.tsx
@@ -9,7 +9,7 @@ switch (window.location.hostname) {
         IMGURL = 'http://localhost:3001';
         break;
     // this is the deployed react application
-    case 'www.runjournal.fit':
+    case 'myrunjournal.herokuapp.com':
         // this is the full url of your deployed API
         APIURL = 'https://runjournal-server.herokuapp.com';
         IMGURL = 'https://www.runjournal.fit';
